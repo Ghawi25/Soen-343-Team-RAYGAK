99a47d6dcb06cbc7e562c62150a21220ce7fe70b
Yash Patel <yashpatel1815@hotmail.com>
2024-03-12 18:22:05 -0400

    Created house controller

--- server/src/main/java/com/raygak/server/controllers/HouseController.java
+++ server/src/main/java/com/raygak/server/controllers/HouseController.java
@@ -1,5 +1,20 @@
 package com.raygak.server.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+// Imports
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.raygak.server.services.HouseService;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/house")
 public class HouseController {
-    
+    private final HouseService houseService;
+
+    @Autowired
+    public HouseController(HouseService houseService) {
+        userService =
+    }
 }
