18eeea42cc31ca1c00d074519aa4dc3add37df63
Khanh Huy Nguyen <huy2272@gmail.com>
2024-03-11 20:49:56 -0400

    HouseView is now instantiating Rooms

--- server/src/main/java/com/raygak/server/smarthome/HouseView.java
+++ server/src/main/java/com/raygak/server/smarthome/HouseView.java
@@ -1,10 +1,37 @@
 package com.raygak.server.smarthome;
 
+import com.raygak.server.smarthome.heating.House;
+import com.raygak.server.smarthome.heating.Room;
+import lombok.Getter;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public final class HouseView {
     private static HouseView home;
+    public House house;
+    ArrayList<Room> rooms = new ArrayList<>();
+
     private HouseView() {
         // Init our home instance here
         // Calls file-reader method and initialize the house.
+        Layout layout = new Layout();
+        JSONObject houseObj = layout.getLayoutJSON();
+        JSONArray roomsArr = (JSONArray) houseObj.get("rooms");
+        Iterator<JSONObject> iterator = roomsArr.iterator();
+        while (iterator.hasNext()) {
+            JSONObject room = iterator.next();
+            String id = (String) room.get("_id");
+            String name = (String) room.get("name");
+            int width = Integer.parseInt(room.get("width").toString());
+            int height = Integer.parseInt(room.get("height").toString());
+            Boolean light = (Boolean) room.get("lightInRoom");
+            Room newRoom = new Room(id, name, width, height, light);
+            rooms.add(newRoom);
+        }
+        house = new House(rooms);
     }
 
     public static HouseView getHome() {
