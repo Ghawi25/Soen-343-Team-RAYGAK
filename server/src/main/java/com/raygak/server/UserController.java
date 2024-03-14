faa98c8c35964e5db0a9a5e757aaedda63181e47
Yash Patel <yashpatel1815@hotmail.com>
2024-03-14 17:01:26 -0400

    Use case 2, 3, 4

--- server/src/main/java/com/raygak/server/UserController.java
+++ server/src/main/java/com/raygak/server/UserController.java
@@ -30,17 +30,18 @@ public class UserController {
         return ResponseEntity.ok().body(user);
     }
     // create a user
+    // Use case 2 - Create user profile
     @PostMapping
     public User createUserProfile(@RequestBody User user) {
         return userService.createUserProfile(user);
     }
-    // update a user
+    // Use case 3 - Edit user profile
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUserProfile(@PathVariable(value = "id") String id, @RequestBody User userDetails) {
         User updatedUserProfile = userService.updateUserProfile(id, userDetails);
         return ResponseEntity.ok(updatedUserProfile);
     }
-    // delete a user
+    // Use case 4 - Delele user profile
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUserProfile(@PathVariable(value = "id") String id) {
         userService.deleteUserProfile(id);
