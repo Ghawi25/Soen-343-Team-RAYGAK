3b2fe519c00cbeb078934d3d471710bdeaf88fe1
Yash Patel <yashpatel1815@hotmail.com>
2024-03-25 13:42:13 -0400

    Made username a unique field

--- back-end/src/main/java/com/raygak/server/models/User.java
+++ back-end/src/main/java/com/raygak/server/models/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
@@ -14,6 +15,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class User {
     @Id
     private ObjectId id;
+    @Indexed(unique = true)
     private String username;
     private String password;
     private String userType; // adult, child, guest
