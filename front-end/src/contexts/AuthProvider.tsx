0254f4171d70e0c8c9fcee1a873865d28d19ed0d
Yash Patel <yashpatel1815@hotmail.com>
2024-03-25 13:33:53 -0400

    Implemented login and logout for the authprovider

--- front-end/src/contexts/AuthProvider.tsx
+++ front-end/src/contexts/AuthProvider.tsx
@@ -1,9 +1,20 @@
-import { createContext, useContext } from "react";
+import axios from "axios";
+import { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext({});
 
 const AuthProvider = ({children}) => {
-    return <AuthContext.Provider>{children}</AuthContext.Provider>;
+    const [user, setUser] = useState(null);
+    
+    const loginAction = async (data) => {
+        const res = await axios.get(`http://localhost:8080/api/users/user/${data.username}`);
+        setUser(res.data);
+    }
+
+    const logOut = () => {
+        setUser(null);
+    }
+    return <AuthContext.Provider value={{user, loginAction, logOut}}>{children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
