00c7af90ae139755ab1d83485f3d0aee870bff33
Yash Patel <yashpatel1815@hotmail.com>
2024-03-24 21:47:13 -0400

    Revert "resolving merge conflict"
    This reverts commit ed563550be42cf34cc8ccf9da42a05fd7d0b16b7, reversing
    changes made to 6e8ff86610b0e816d7ebecae46bd51087e65599a.

--- server/src/main/java/com/raygak/server/commands/Command.java
+++
@@ -1,5 +0,0 @@
-package com.raygak.server.commands;
-
-public abstract class Command {
-    public abstract void execute();
-}
--- server/src/main/java/com/raygak/server/smarthome/UserType.java
+++
@@ -1,5 +0,0 @@
-package com.raygak.server.smarthome;
-
-public enum UserType {
-    PARENT, CHILD, GUEST, STRANGER
-}
