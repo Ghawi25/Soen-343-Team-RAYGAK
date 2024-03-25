8f62b7e5c3c0bf821a3706113b3dc6e53b514fb3
Khanh Huy Nguyen <huy2272@gmail.com>
2024-03-25 16:17:35 -0400

    Integrated console logs api to display data on the frontend

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import AuthProvider from "./contexts/AuthProvider";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
     <div id="dashboard">
       <AuthProvider>
-        <Dashboard />
+        <QueryClientProvider client={queryClient}>
+          <Dashboard />
+        </QueryClientProvider>
       </AuthProvider>
     </div>
   );
--- front-end/src/components/OutputConsole/OuputBox.tsx
+++ front-end/src/components/OutputConsole/OuputBox.tsx
@@ -1,5 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 function OutputBox() {
-  return <div>this will be the output</div>;
+  // API logic
+  const getLogs = async () => {
+    const response = await axios.get(
+      `http://localhost:8080/api/house/temperature`
+    );
+    return response.data;
+  };
+  // Queries
+  const { data } = useQuery({
+    queryKey: ["logs"],
+    queryFn: getLogs,
+    refetchInterval: 1000,
+  });
+
+  return (
+    <div>
+      <ul>
+        {data?.logMsg.map((log: string, index: number) => (
+          <li key={index}>{log}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default OutputBox;
