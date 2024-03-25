2191ea28c24bafb94888aae52ad58d16011b42f4
Yash Patel <yashpatel1815@hotmail.com>
2024-03-25 13:33:39 -0400

    Added different visibility if logged in

--- front-end/src/components/SH/SHS.tsx
+++ front-end/src/components/SH/SHS.tsx
@@ -1,41 +1,54 @@
-import axios from "axios";
 import { useState } from "react";
+import { userAuth } from "../../contexts/AuthProvider";
 
 function SHS() {
+  const auth = userAuth();
   const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");  
+  const [password, setPassword] = useState("");
 
-  const handleSubmit = (e: { preventDefault: () => void; }) => {
+  const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
-    axios.get(`http://localhost:8080/api/users/user/${username}`)
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => console.log(err));
-  }
+    const data = {
+      username: username,
+      password: password,
+    };
+    auth.loginAction(data);
+  };
 
-  return (
-    <>
-      <form
-        id="login"
-        className="bubble"
-        onSubmit={handleSubmit}
-      >
-        <h5>Sign-In</h5>
-        <div className="field">
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" required onChange={(e) => setUsername(e.target.value)}/>
-        </div>
-        <div className="field">
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required onChange={(e) => setPassword(e.target.value)}/>
-        </div>
-        <div>
-          <button type="submit">Sign In</button>
-        </div>
-      </form>
-    </>
-  );
+  if (auth.user) {
+    return <div>Logged in as {auth.user.username}</div>;
+  } else {
+    return (
+      <>
+        <form id="login" className="bubble" onSubmit={handleSubmit}>
+          <h5>Sign-In</h5>
+          <div className="field">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              required
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <button type="submit">Sign In</button>
+          </div>
+        </form>
+      </>
+    );
+  }
 }
 
 export default SHS;
