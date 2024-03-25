d820c2b7e11c7a064fa2c4cb5831adf482491078
Yash Patel <yashpatel1815@hotmail.com>
2024-03-25 17:56:03 -0400

    Sending request to get house layout information

--- front-end/src/components/HouseView/HouseBox.tsx
+++ front-end/src/components/HouseView/HouseBox.tsx
@@ -1,4 +1,17 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function HouseBox() {
+  const [house, setHouse] = useState(null);
+
+  // Get house information
+  useEffect(() => {
+    axios
+      .get("localhost:8080/ap/house")
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
+
   return <div>HouseBox</div>;
 }
 
