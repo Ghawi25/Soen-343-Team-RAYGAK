083a3df819ff08c87ca894e7ec0181549912dd5d
Yash Patel <yashpatel1815@hotmail.com>
2024-03-26 14:05:59 -0400

    Adjacent room of current rooms are saved in an array

--- front-end/src/components/HouseView/HouseBox.tsx
+++ front-end/src/components/HouseView/HouseBox.tsx
@@ -1,16 +1,21 @@
-import { render } from "@testing-library/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 // function to generate a rectangle for the room
 const Room = ({ x, y, width, height }) => {
-  return <rect x={x} y={y} width={width} height={height} fill="black"/>
+  return <rect x={x} y={y} width={width*100} height={height*100} fill="black"/>
 }
 
 // Function to render all rooms
 const renderRooms = (rooms) => {
-  rooms.map((room) => {
-    
+  const visitedRooms = [];
+  return rooms.map((room) => {
+    if(room in visitedRooms) return;
+    visitedRooms.push(room);
+    const adjacentRooms = []
+    adjacentRooms.push({top: room.topAdjacentRoom, bottom: room.bottomAdjacentRoom, left: room.leftAdjacentRoom, right: room.rightAdjacentRoom})
+    console.log(adjacentRooms)
+    return <Room x={0} y={0} width={room.width} height={room.height}/>
   })
 }
 
