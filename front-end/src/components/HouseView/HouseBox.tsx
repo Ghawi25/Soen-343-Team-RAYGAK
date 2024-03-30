1ad7d0bbc69c9ff66a597214ae54f0a44e5a3db7
Yash Patel <yashpatel1815@hotmail.com>
2024-03-30 09:37:54 -0400

    Widnows can be generated. Automatically spaced evenly

--- front-end/src/components/HouseView/HouseBox.tsx
+++ front-end/src/components/HouseView/HouseBox.tsx
@@ -1,46 +1,86 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-var roomPositions = {}
+var roomPositions = {};
 // function to generate a rectangle for the room
 const Room = ({ name, x, y, width, height, id }: any) => {
   return (
     <g key={id} id={id}>
-      <rect x={x * width * 100} y={y * height * 100} width={width * 100} height={height * 100} fill="white" stroke="black" />
-      <text x={x * width * 100 + width * 10} y={y * height * 100 + height * 50} font-family="Verdana" font-size="35" fill="black">{name}</text>
-    </g>)
-}
+      <rect
+        x={x * width * 100}
+        y={y * height * 100}
+        width={width * 100}
+        height={height * 100}
+        fill="white"
+        stroke="black"
+      />
+      <text
+        x={x * width * 100 + width * 10}
+        y={y * height * 100 + height * 50}
+        font-family="Verdana"
+        font-size="35"
+        fill="black"
+      >
+        {name}
+      </text>
+    </g>
+  );
+};
 
 // function to generate doors
 const Door = ({ name, x, y, roomWidth, roomHeight, isVertical }: any) => {
   if (isVertical) {
     return (
       <g key={name} id={name}>
-        <rect x={x * roomWidth * 100 - 5} y={y * roomHeight * 100 - 12.5} width={10} height={25} fill="rgb(139,69,19)" />
+        <rect
+          x={x * roomWidth * 100 - 5}
+          y={y * roomHeight * 100 - 12.5}
+          width={10}
+          height={25}
+          fill="rgb(139,69,19)"
+        />
       </g>
-    )
+    );
   }
   return (
     <g key={name} id={name}>
-      <rect x={x * roomWidth * 100 - 12.5} y={y * roomHeight * 100 - 5} width={25} height={10} fill="rgb(139,69,19)" />
+      <rect
+        x={x * roomWidth * 100 - 12.5}
+        y={y * roomHeight * 100 - 5}
+        width={25}
+        height={10}
+        fill="rgb(139,69,19)"
+      />
     </g>
-  )
-}
+  );
+};
 
 const Window = ({ name, x, y, roomWidth, roomHeight, isVertical }: any) => {
   if (isVertical) {
     return (
       <g key={name} id={name}>
-        <rect x={x * roomWidth * 100 - 5} y={y * roomHeight * 100 - 12.5} width={10} height={25} fill="rgb(135,206,235)" />
+        <rect
+          x={x * roomWidth * 100 - 5}
+          y={y * roomHeight * 100 - 12.5}
+          width={10}
+          height={25}
+          fill="rgb(135,206,235)"
+        />
       </g>
-    )
+    );
   }
   return (
     <g key={name} id={name}>
-      <rect x={x * roomWidth * 100 - 12.5} y={y * roomHeight * 100 - 5} width={25} height={10} fill="rgb(135,206,235)" />
+      <rect
+        x={x * roomWidth * 100 - 12.5}
+        y={y * roomHeight * 100 - 5}
+        width={25}
+        height={10}
+        fill="rgb(135,206,235)"
+      />
     </g>
-  )
-}
+  );
+};
 
 // Function to render all rooms
 function renderRooms(rooms: any[]) {
@@ -49,43 +89,64 @@ function renderRooms(rooms: any[]) {
   // Start at 0, 0
   var x = 0;
   var y = 0;
-  roomPositions[rooms[0].roomID] = { x: x, y: y }
+  roomPositions[rooms[0].roomID] = { x: x, y: y };
   rooms.forEach((room) => {
     var currRoomPositions = roomPositions[room.roomID];
     var adjacentRoom = room.topAdjacentRoom;
     if (!(adjacentRoom === null)) {
-      roomPositions[adjacentRoom.roomID] = { x: currRoomPositions.x, y: currRoomPositions.y - 1 }
+      roomPositions[adjacentRoom.roomID] = {
+        x: currRoomPositions.x,
+        y: currRoomPositions.y - 1,
+      };
     }
     adjacentRoom = room.bottomAdjacentRoom;
     if (!(adjacentRoom === null)) {
-      roomPositions[adjacentRoom.roomID] = { x: currRoomPositions.x, y: currRoomPositions.y + 1 }
+      roomPositions[adjacentRoom.roomID] = {
+        x: currRoomPositions.x,
+        y: currRoomPositions.y + 1,
+      };
     }
     adjacentRoom = room.leftAdjacentRoom;
     if (!(adjacentRoom === null)) {
-      roomPositions[adjacentRoom.roomID] = { x: currRoomPositions.x - 1, y: currRoomPositions.y }
+      roomPositions[adjacentRoom.roomID] = {
+        x: currRoomPositions.x - 1,
+        y: currRoomPositions.y,
+      };
     }
     adjacentRoom = room.rightAdjacentRoom;
     if (!(adjacentRoom === null)) {
-      roomPositions[adjacentRoom.roomID] = { x: currRoomPositions.x + 1, y: currRoomPositions.y }
+      roomPositions[adjacentRoom.roomID] = {
+        x: currRoomPositions.x + 1,
+        y: currRoomPositions.y,
+      };
     }
-  })
+  });
   // calculating offsets to make turn all positions positive
   var minX = Infinity;
   var minY = Infinity;
   for (let [key, value] of Object.entries(roomPositions)) {
     if (value.x < minX) minX = value.x;
     if (value.y < minY) minY = value.y;
-  };
+  }
 
   // apply offset
   for (let [key, value] of Object.entries(roomPositions)) {
     value.x = value.x - minX;
     value.y = value.y - minY;
-  };
+  }
 
-  return rooms.map(room => {
+  return rooms.map((room) => {
     const roomPosition = roomPositions[room.roomID];
-    return <Room name={room.name} x={roomPosition.x} y={roomPosition.y} width={room.width} height={room.height} id={room.roomID} />
+    return (
+      <Room
+        name={room.name}
+        x={roomPosition.x}
+        y={roomPosition.y}
+        width={room.width}
+        height={room.height}
+        id={room.roomID}
+      />
+    );
   });
 }
 
@@ -94,81 +155,147 @@ function renderDoors(rooms: any[]) {
   var doorPositions = {};
 
   // get offset of doors
-  rooms.forEach(room => {
+  rooms.forEach((room) => {
     const roomPosition = roomPositions[room.roomID];
     const doors = room.doors;
-    doors.forEach((door: { position: string; name: string; }) => {
+    doors.forEach((door: { position: string; name: string }) => {
       if (door.position === "north") {
-        doorPositions[door.name] = { x: roomPosition.x + 0.5, y: roomPosition.y, isVertical: false };
-      }
-      else if (door.position === "south") {
-        doorPositions[door.name] = { x: roomPosition.x + 0.5, y: roomPosition.y + 1, isVertical: false };
-      }
-      else if (door.position === "east") {
-        doorPositions[door.name] = { x: roomPosition.x + 1, y: roomPosition.y + 0.5, isVertical: true };
-      }
-      else {
-        doorPositions[door.name] = { x: roomPosition.x, y: roomPosition.y + 0.5, isVertical: true };
+        doorPositions[door.name] = {
+          x: roomPosition.x + 0.5,
+          y: roomPosition.y,
+          isVertical: false,
+        };
+      } else if (door.position === "south") {
+        doorPositions[door.name] = {
+          x: roomPosition.x + 0.5,
+          y: roomPosition.y + 1,
+          isVertical: false,
+        };
+      } else if (door.position === "east") {
+        doorPositions[door.name] = {
+          x: roomPosition.x + 1,
+          y: roomPosition.y + 0.5,
+          isVertical: true,
+        };
+      } else {
+        doorPositions[door.name] = {
+          x: roomPosition.x,
+          y: roomPosition.y + 0.5,
+          isVertical: true,
+        };
       }
     });
   });
 
   // Render doors, skip if already rendered
   const added = new Set();
-  return rooms.map(room => {
+  return rooms.map((room) => {
     const doors = room.doors;
-    return doors.map((door: { name: string; }) => {
-      const doorPosition = doorPositions[door.name]
+    return doors.map((door: { name: string }) => {
+      const doorPosition = doorPositions[door.name];
       if (added.has(door.name)) return;
-      added.add(door.name)
-      if (doorPosition.isVertical) {
-        return <Door name={door.name} x={doorPosition.x} y={doorPosition.y} roomWidth={room.width} roomHeight={room.height} isVertical={doorPosition.isVertical} />
-      }
-      else {
-        return <Door name={door.name} x={doorPosition.x} y={doorPosition.y} roomWidth={room.width} roomHeight={room.height} isVertical={doorPosition.isVertical} />
-      }
-    })
-  })
+      added.add(door.name);
+      return (
+        <Door
+          name={door.name}
+          x={doorPosition.x}
+          y={doorPosition.y}
+          roomWidth={room.width}
+          roomHeight={room.height}
+          isVertical={doorPosition.isVertical}
+        />
+      );
+    });
+  });
 }
 
 // render windows
 function renderWindows(rooms: any[]) {
   var windowPositions = {};
 
-  rooms.forEach(room => {
+  rooms.map((room) => {
     const roomPosition = roomPositions[room.roomID];
     const windows = room.windows;
     // For each room, get windows per wall
     const windowsPerDirection = {
-      "north": [],
-      "south": [],
-      "east": [],
-      "west": []
+      north: [],
+      south: [],
+      east: [],
+      west: [],
     };
 
-    windows.forEach(window => {
-      console.log(window);
+    windows.forEach((window) => {
       if (window.position === "north") {
         windowsPerDirection["north"].push(window);
-      }
-      else if (window.position === "south") {
+      } else if (window.position === "south") {
         windowsPerDirection["south"].push(window);
-      }
-      else if (window.position === "east") {
+      } else if (window.position === "east") {
         windowsPerDirection["east"].push(window);
-      }
-      else {
+      } else {
         windowsPerDirection["west"].push(window);
       }
     });
-    
-    // Get positions of windows 
-    windows.forEach(window => {
-      
+
+    // Get positions of windows
+    let i = 1;
+    windowsPerDirection["south"].forEach((window) => {
+      windowPositions[window.windowID] = {
+        x: roomPosition.x + (i * 1) / (windowsPerDirection["south"].length + 1),
+        y: roomPosition.y + 1,
+        isVertical: false,
+      };
+      i++;
+    });
+    i = 1;
+    windowsPerDirection["north"].forEach((window) => {
+      windowPositions[window.windowID] = {
+        x: roomPosition.x + (i * 1) / (windowsPerDirection["north"].length + 1),
+        y: roomPosition.y,
+        isVertical: false,
+      };
+      i++;
+    });
+    i = 1;
+    windowsPerDirection["east"].forEach((window) => {
+      windowPositions[window.windowID] = {
+        x: roomPosition.x + 1,
+        y: roomPosition.y + (i * 1) / (windowsPerDirection["east"].length + 1),
+        isVertical: true,
+      };
+      i++;
+    });
+    i = 1;
+    windowsPerDirection["west"].forEach((window) => {
+      windowPositions[window.windowID] = {
+        x: roomPosition.x,
+        y: roomPosition.y + (i * 1) / (windowsPerDirection["west"].length + 1),
+        isVertical: true,
+      };
+      i++;
+    });
+    console.log(windowPositions);
+  });
+  // Rendering the windows:
+  const added = new Set();
+  return rooms.map((room) => {
+    const windows = room.windows;
+    return windows.map((window: { windowID: string }) => {
+      const windowPosition = windowPositions[window.windowID];
+      if (added.has(window.windowID)) return;
+      added.add(window.windowID);
+      return (
+        <Window
+          name={window.windowID}
+          x={windowPosition.x}
+          y={windowPosition.y}
+          roomWidth={room.width}
+          roomHeight={room.height}
+          isVertical={windowPosition.isVertical}
+        />
+      );
     });
   });
 }
-
 
 // TODO render lights
 
@@ -184,7 +311,7 @@ function HouseBox() {
   }, []);
 
   if (!house) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   } else {
     return (
       <svg width="2000" height="2000">
@@ -192,7 +319,7 @@ function HouseBox() {
         {renderDoors(house.rooms)}
         {renderWindows(house.rooms)}
       </svg>
-    )
+    );
   }
 }
 
