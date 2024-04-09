cc2d3171d3c809832f85cb0b537cab3112c6d8a2
Yash Patel <yashpatel1815@hotmail.com>
2024-04-09 09:15:09 -0400

    Added photoservice to create methods that can be used in endpoints

---
+++ back-end/src/main/java/com/raygak/server/services/PhotoService.java
@@ -0,0 +1,37 @@
+package com.raygak.server.services;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.bson.BsonBinarySubType;
+import org.bson.types.Binary;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.raygak.server.models.Photo;
+import com.raygak.server.repos.PhotoRepository;
+
+@Service
+public class PhotoService {
+    
+    @Autowired
+    private PhotoRepository photoRepository;
+
+    public Optional<Photo> getPhotoByUsername(String username) {
+        return photoRepository.findByUsername(username);
+    }
+
+    public Optional<Photo> getPhotoByUsername(ObjectId id) {
+        return photoRepository.findById(id);
+    }
+
+    public Photo addPhoto(String username, MultipartFile file) throws IOException {
+        Photo photo = new Photo();
+        photo.setUsername(username);
+        photo.setImage(new Binary(BsonBinarySubType.BINARY, file.getBytes()));
+        photoRepository.insert(photo);
+        return photo;
+    }
+}
